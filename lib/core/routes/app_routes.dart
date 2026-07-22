e5ddb770475c827ccff9462409caa6ad1c399bd8
Mostafaali <mostafa@gmail.com>
2026-07-22 23:40:28 +0300

    Add AppRoutes class with route definitions for navigation

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,6 @@
+abstract class AppRoutes {
+  static const String homeRoute = '/home';
+  static const String cartRoute = '/cart';
+  static const String favRoute = '/fav';
+  static const String accountRoute = '/account';
+}
