4d891c79244a24c695426904fc59937739167497
Mostafaali <mostafa@gmail.com>
2026-07-22 19:38:00 +0300

    Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:nti_graduation_project/core/theme/theme_app.dart';
+
+void main() {
+  runApp(const ShoppingApp());
+}
+
+class ShoppingApp extends StatelessWidget {
+  const ShoppingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeApp.lightTheme,
+      themeMode: .light,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {},
+    );
+  }
+}
