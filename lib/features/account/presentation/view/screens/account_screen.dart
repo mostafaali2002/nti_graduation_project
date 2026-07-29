3997c3ad0b1680757fb7ac286fe5ee564e112b0e
fathy <fathyaboesmael.com>
2026-07-29 20:46:32 +0300

    Refactor screen classes to standardize naming conventions and improve readability

--- lib/features/account/presentation/view/screens/account_screen.dart
+++ lib/features/account/presentation/view/screens/account_screen.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/routes/app_routes.dart';
 
-class Account extends StatelessWidget {
-  const Account({super.key});
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key});
   static const routeName = AppRoutes.accountRoute;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Account Screen"),),
-    );
+    return Scaffold(body: Center(child: Text("Account Screen")));
   }
 }
--- lib/features/cart/presentation/view/screens/cart_screen.dart
+++ lib/features/cart/presentation/view/screens/cart_screen.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/routes/app_routes.dart';
 
-class Cart extends StatelessWidget {
-  const Cart({super.key});
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
   static const routeName = AppRoutes.cartRoute;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       body: Center(child: Text("Cart Screen"),),
-    );
+    return Scaffold(body: Center(child: Text("Cart Screen")));
   }
 }
