8015d9dc673eafb9dadb803f4d07ba7a591f86fc
Mostafaali <mostafa@gmail.com>
2026-07-31 10:48:53 +0300

    Initialize DioHelper in getAccountInfo and postAccountInfo methods

--- lib/features/account/data/repo/account_data_source_imp.dart
+++ lib/features/account/data/repo/account_data_source_imp.dart
@@ -16,6 +16,7 @@ class AccountDataSourceImp implements AccountDataSourceInterface {
   @override
   Future<ResultApi<GetProfileEntity>> getAccountInfo() async {
     try {
+      DioHelper.init();
       final response = await DioHelper.dio.get(
         ApiConstant.accountEndPointGet,
         options: Options(
@@ -43,6 +44,7 @@ class AccountDataSourceImp implements AccountDataSourceInterface {
       address: profileEntity.address,
     );
     try {
+      DioHelper.init();
       final response = await DioHelper.dio.post(
         data: messageDto.toJson(),
         ApiConstant.accountEndPointPost,
